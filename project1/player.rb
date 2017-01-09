a014cf6bcbc68e63dddd14a7cd250ada93007a50
nicoasp <nicolas.amaya@gmail.com>
2017-01-09 16:57:40 +0100

    add check user input

--- project1/player.rb
+++ project1/player.rb
@@ -8,6 +8,10 @@ class Player
 	def make_play
 		puts "Your turn #{@name}! Choose a square! (a1 / a3 / c2...)"
 		position = gets.chomp.downcase
+		unless position.match /[abc][123]/
+			puts "That's not a valid square! You lose your turn!"
+			sleep 1
+		end
 		position
 	end
 end
